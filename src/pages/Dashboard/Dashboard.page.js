5d1cc0cb7e1552c4e5b85953b3608c75c73539ff
Dave Pettit <Dave Pettit@DESKTOP-RVSSCN7>
2021-04-25 14:15:26 -0400

    breadcrumb added

--- src/pages/Dashboard/Dashboard.page.js
+++ src/pages/Dashboard/Dashboard.page.js
@@ -2,10 +2,16 @@ import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { TicketTable } from '../../components/ticket-table/TicketTable.comp';
 import tickets from '../../assets/data/dummy-tickets.json';
+import {PageBreadcrumb} from '../../components/breadcrumb/Breadcrum.comp'
 
 export const Dashboard = () => {
   return (
-    <Container>
+      <Container>
+          <Row>
+              <Col>
+              <PageBreadcrumb page="Dashboard" />
+              </Col>
+          </Row>
       <Row>
         <Col className="text-center mt-5 mb-2">
           <Button
